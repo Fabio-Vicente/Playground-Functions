457bd69e0e6303b4a31dd40b54f6995ebd0e4fd0
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-17 12:54:30 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -24,8 +24,8 @@ function concatName(arr) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
